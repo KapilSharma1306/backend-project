c929b60828b4d16a15ae9cbc3f9fbd070d93b6c9
KapilSharma1306 <kapilSharmacoding1306202@gmail.com>
2024-11-04 05:49:02 +0530

    debug in user.model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -45,11 +45,11 @@ const userSchema = new Schema({
 }, {timestamps:true})
 
 userSchema.pre("save" , async function(next) {
-  if(this.isModified("password")) // it checks that password is modify or not or if(!this.isModified("password")) return next()
-    {
-        this.password = bcrypt.hash(this.password,10)
+  if(!this.isModified("password"))  return next() // it checks that password is modify or not 
+    
+        this.password = await bcrypt.hash(this.password,10)
         next()
-    }
+    
 })
 userSchema.methods.isPasswordCorrect = async function (password) {
     await bcrypt.compare(password,this.password)
